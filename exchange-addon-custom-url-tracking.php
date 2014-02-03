0c0873d0f6be4051f7f6e50d813b3c632a22f9b5
Git User <git@petra.ithemes.com>
2014-02-03 16:33:00 -0600

    1.0.4: updated: lib/updater

--- exchange-addon-custom-url-tracking.php
+++ exchange-addon-custom-url-tracking.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Custom URL Tracking
- * Version: 1.0.3
+ * Version: 1.0.4
  * Description: Allows you to add custom URLs to products and to track usage.
  * Plugin URI: http://ithemes.com/purchase/custom-urls-add-on/
  * Author: iThemes
