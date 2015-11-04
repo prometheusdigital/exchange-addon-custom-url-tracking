9a2e1622871eb52b33c9e159162f121ced24486a
Lew Ayotte <lew@ithemes.com>
2015-11-04 13:55:41 -0600

    Tagging 1.2.0

--- exchange-addon-custom-url-tracking.php
+++ exchange-addon-custom-url-tracking.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Custom URL Tracking
- * Version: 1.1.2
+ * Version: 1.2.0
  * Description: Allows you to add custom URLs to products and to track usage.
  * Plugin URI: http://ithemes.com/purchase/custom-urls-add-on/
  * Author: iThemes
