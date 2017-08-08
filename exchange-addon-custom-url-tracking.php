8367aac72a4bb2ee6c4a4736a46410bb44a33f3a
AJ Morris <ajmorris2002@gmail.com>
2017-08-07 23:38:50 -0400

    Putting things in place for the updater.
    Rebranded complete as well

--- exchange-addon-custom-url-tracking.php
+++ exchange-addon-custom-url-tracking.php
@@ -1,13 +1,13 @@
 <?php
 /*
- * Plugin Name: iThemes Exchange - Custom URL Tracking
- * Version: 1.2.0
+ * Plugin Name: ExchangeWP - Custom URL Tracking
+ * Version: 0.2.1
  * Description: Allows you to add custom URLs to products and to track usage.
- * Plugin URI: http://ithemes.com/purchase/custom-urls-add-on/
- * Author: iThemes
- * Author URI: http://ithemes.com
- * iThemes Package: exchange-addon-custom-url-tracking
- 
+ * Plugin URI: https://exchangewp.com/downloads/custom-url-tracking/
+ * Author: ExchangeWP
+ * Author URI: https://exchangewp.com
+ * ExchangeWP Package: exchange-addon-custom-url-tracking
+
  * Installation:
  * 1. Download and unzip the latest release zip file.
  * 2. If you use the WordPress plugin uploader to install this plugin skip to step 4.
@@ -27,12 +27,13 @@ function it_exchange_register_custom_url_tracking_addon() {
 	$options = array(
 		'name'              => __( 'Custom URL Tracking', 'LION' ),
 		'description'       => __( 'Allows you to add custom URLs to products and to track usage.', 'LION' ),
-		'author'            => 'iThemes',
-		'author_url'        => 'http://ithemes.com/purchase/custom-urls-add-on/',
+		'author'            => 'ExchangeWP',
+		'author_url'        => 'https://exchangewp.com/downloads/custom-url-tracking/',
 		'icon'              => ITUtility::get_url_from_file( dirname( __FILE__ ) . '/lib/images/custom-url-tracking-50px.png' ),
 		'file'              => dirname( __FILE__ ) . '/init.php',
 		'category'          => 'product-feature',
 		'basename'          => plugin_basename( __FILE__ ),
+		'settings-callback' => 'it_exchange_custom_url_tracking_addon_settings_callback',
 	);
 	it_exchange_register_addon( 'custom-url-tracking', $options );
 }
@@ -58,8 +59,40 @@ add_action( 'plugins_loaded', 'it_exchange_custom_url_tracking_set_textdomain' )
  * @param object $updater ithemes updater object
  * @return void
 */
-function ithemes_exchange_addon_custom_url_tracking_updater_register( $updater ) { 
+function ithemes_exchange_addon_custom_url_tracking_updater_register( $updater ) {
 	    $updater->register( 'exchange-addon-custom-url-tracking', __FILE__ );
 }
 add_action( 'ithemes_updater_register', 'ithemes_exchange_addon_custom_url_tracking_updater_register' );
-require( dirname( __FILE__ ) . '/lib/updater/load.php' );
+// require( dirname( __FILE__ ) . '/lib/updater/load.php' );
+
+
+if ( ! class_exists( 'EDD_SL_Plugin_Updater' ) )  {
+	require_once 'EDD_SL_Plugin_Updater.php';
+}
+
+function exchange_custom_url_tracking_plugin_updater() {
+
+	// retrieve our license key from the DB
+	// this is going to have to be pulled from a seralized array to get the actual key.
+	// $license_key = trim( get_option( 'exchange_custom_url_tracking_license_key' ) );
+	// $exchangewp_custom_url_tracking_options = get_option( 'it-storage-exchange_custom_url_tracking-addon' );
+	// $license_key = $exchangewp_custom_url_tracking_options['custom_url_tracking-license-key'];
+	$license_key = "";
+
+	// setup the updater
+	$edd_updater = new EDD_SL_Plugin_Updater( 'https://exchangewp.com', __FILE__, array(
+			'version' 		=> '0.2.0', 				// current version number
+			'license' 		=> $license_key, 		// license key (used get_option above to retrieve from DB)
+			'item_name' 	=> urlencode( 'custom-url-tracking' ), 	  // name of this plugin
+			'author' 	  	=> 'ExchangeWP',    // author of this plugin
+			'url'       	=> home_url(),
+			'wp_override' => true,
+			'beta'		  	=> false
+		)
+	);
+	// var_dump($edd_updater);
+	// die();
+
+}
+
+add_action( 'admin_init', 'exchange_custom_url_tracking_plugin_updater', 0 );
